d63eb82c5277dc9c8069334752b073e16c31250b
Moujib <Moujibch10@gmail.com>
2026-03-31 09:57:24 +0100

    resolve conflict

---
+++ lib/app/pages/main/home.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/get_core.dart';
+import 'package:project/app/controllers/main/productController/CartController.dart';
+import 'package:project/app/controllers/main/productController/ProductController.dart';
+import 'package:project/app/pages/components/Products_components.dart';
+import 'package:project/app/pages/components/elementsBar_component.dart';
+import 'package:project/app/pages/components/formField_component.dart';
+import 'package:project/app/pages/components/image_Component.dart';
+import 'package:project/app/pages/components/space_component.dart';
+import 'package:project/app/pages/components/text_component.dart';
+import 'package:project/utils/colors.dart';
+
+
+
+class HomePage extends StatelessWidget {
+  HomePage({super.key});
+
+  final controller = Get.put(ProductController());
+  final cartController = Get.put(CartController());
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(height: 30),
+
+                _buildHeader(),
+
+                h(20),
+
+                _buildCategorySelector(),
+
+                h(20),
+
+                _buildBanner(),
+
+                h(10),
+
+                _buildSectionHeader("Categories"),
+
+                _buildTypeList(),
+
+                h(10),
+
+                _buildSectionHeader("All Product"),
+
+                _buildProductGrid(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHeader() {
+  return Row(
+    children: [
+      Expanded(
+        child: Container(
+          height: 60,
+          padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+          decoration: BoxDecoration(
+            color: grey,
+            borderRadius: BorderRadius.circular(100),
+          ),
+          child: FormfieldComponent(
+            controller:  controller.searchController,
+            hintText: "Search your product",
+            prefixIcon: const Icon(Icons.search),
+            border: false,
+          ),
+        ),
+      ),
+      const SizedBox(width: 10),
+      CircleAvatar(
+        radius: 25,
+        backgroundColor: grey,
+        child: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.shopping_bag_outlined),
+        ),
+      )
+    ],
+  );
+}
+Widget _buildCategorySelector() {
+  return Row(
+    children: [
+   Obx(() => ElementsbarComponent(
+  list: controller.categories,
+  selectedIndex: controller.selectedIndex.value,
+  onSelect: controller.selectCategory,
+)),
+        IconButton(
+        onPressed: () {
+          Get.toNamed('/filter');
+        },
+        icon: const Icon(Icons.settings),
+      )
+    ],
+  );
+}
+
+Widget _buildBanner() {
+  return ImageComponent(
+    taille: 12,
+    chemin: "assets/images/home.jpg",
+  );
+}
+Widget _buildSectionHeader(String title) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      TextComponent(
+        txt: title,
+        fw: FontWeight.bold,
+        family: "Bold",
+      ),
+      TextButton(
+        onPressed: () {
+           // REDIRECT PAGE ALL
+        },
+        child: TextComponent(
+          txt: "See all",
+          color: mainColor,
+          fw: FontWeight.bold,
+        ),
+      )
+    ],
+  );
+}
+Widget _buildTypeList() {
+  return SizedBox(
+    height: 100,
+    child: ListView.builder(
+      scrollDirection: Axis.horizontal,
+      itemCount: controller.types.length,
+      itemBuilder: (context, index) {
+        final type = controller.types[index];
+
+        return Padding(
+          padding: const EdgeInsets.only(right: 12),
+          child: CategoryBox(type.name, type.image),
+        );
+      },
+    ),
+  );
+}
+Widget _buildProductGrid() {
+  return Obx(() => Wrap(
+  spacing: 10,
+  runSpacing: 10,
+      children: controller.filteredProducts.map((p) {
+        return productCard(
+          p,
+        () => cartController.addToCartFromHome(p),
+
+          ///  NAVIGATE
+          onTap: () => Get.toNamed('/detail', arguments: p), 
+        );
+      }).toList(),
+));
+}}
